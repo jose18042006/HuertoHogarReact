9465fedb2a6e1a0ddd32636ff18dbbfd9b6d5168
jose18042006 <jo.leiton@duocuc.cl>
2025-12-02 23:27:45 -0300

    Delete HuertoHogarReact-main directory

--- HuertoHogarReact-main/src/components/atoms/Title.jsx
+++
@@ -1,4 +0,0 @@
-export default function Title({ level=1, children, className='' }){
-  const Tag = `h${level}`
-  return <Tag className={className}>{children}</Tag>
-}
--- HuertoHogarReact-main/src/pages/Cambios.jsx
+++
@@ -1,1 +0,0 @@
-export default () => (<main className='form-container'><div className='card'><h2>Cambios</h2></div></main>)
